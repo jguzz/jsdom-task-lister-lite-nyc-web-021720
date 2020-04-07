4777ca5caf3686004694fc99ef3b3e55bf77fb3a
jguzz <joseph.a.guzzardo@gmail.com>
2020-04-07 10:06:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskInput = document.querySelector("input#new-task-description");
+  const taskForm = document.querySelector("form#create-task-form");
+
+  const deleteButton = document.createElement("button");
+  const buttonContainer = document.querySelector("div.button-container");
+  const list = document.querySelector("ul#tasks");
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTask = taskInput.value;
+    const newLi = document.createElement("li");
+
+    newLi.innerText = newTask;
+    list.appendChild(newLi);
+    taskForm.reset();
+  });
+
+  buttonContainer.addEventListener("click", (e) => {
+    console.log("somewhere in the button Container");
+    if (e.target.id === "alert") {
+      console.log("In alert");
+    } else if (e.target.id === "delete") {
+      console.log("this should delete one day");
+      list.childNodes[0].remove();
+    }
+  });
 });
